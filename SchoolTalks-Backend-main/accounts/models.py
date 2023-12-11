d7d416878d227b4ed32c719219cc4e69e4a5689c
Kim chanyang <105031421+Kimchanyang524@users.noreply.github.com>
2023-12-11 16:36:24 +0900

    remove All

--- SchoolTalks-Backend-main/accounts/models.py
+++
@@ -1,8 +0,0 @@
-from django.contrib.auth.models import AbstractUser
-from django.db import models
-
-
-class User(AbstractUser):
-    # 추가 필드 예시
-    about_me = models.TextField(blank=True)  # 사용자의 간단한 자기 소개 정보
-    profile_picture = models.ImageField(upload_to='profile_pics/', blank=True)  # 사용자 프로필 사진을 저장하는 필드
--- SchoolTalks-Backend-main/accounts/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from .views import UserCreateView, UserDetailView
-
-# 로그인은 JWT를 사용하므로, 회원가입과 유저 정보 조회만 구현
-urlpatterns = [
-    path('signup/', UserCreateView.as_view(), name='signup'),  # 회원가입 API 뷰
-    path('user/<int:pk>/', UserDetailView.as_view(), name='user-detail'),  # 유저 정보 조회 API 뷰
-]
