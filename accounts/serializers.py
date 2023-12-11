496de10d16119fff045f3202c7af495c76f91f70
KImchangsu <wave060@naver.com>
2023-12-11 16:59:52 +0900

    🎉 project start

---
+++ accounts/serializers.py
@@ -0,0 +1,14 @@
+from rest_framework import serializers
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ('id', 'email', 'password', 'nickname', 'school_name', 'username', 'grade', 'about_me', 'profile_picture')
+        extra_kwargs = {'password': {'write_only': True}}
+
+    def create(self, validated_data):
+        user = User.objects.create_user(**validated_data)
+        return user
