601f831a7999617cbc71dedc8bef56232d210a19
KCY <cks402@naver.com>
2023-12-13 11:53:33 +0900

    💡 User앱 주석 수정

--- accounts/models.py
+++ accounts/models.py
@@ -1,16 +1,15 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# from django.utils.translation import gettext as _
-
 
 class User(AbstractUser):
-    # 추가 필드 예시
     username = models.CharField(max_length=30, unique=True, verbose_name="아이디")
-    about_me = models.TextField(blank=True, verbose_name="자기소개")  # 사용자의 간단한 자기 소개 정보
+    about_me = models.TextField(blank=True, verbose_name="자기소개")
     profile_picture = models.ImageField(
-        upload_to="profile_pics/", blank=True
-    )  # 사용자 프로필 사진을 저장하는 필드
+        upload_to="profile_pics/",
+        blank=True,
+        verbose_name="프로필 사진",
+    )
     nickname = models.CharField(max_length=20, verbose_name="닉네임")
     school_name = models.CharField(max_length=20, verbose_name="학교명")
     name = models.CharField(max_length=20, verbose_name="이름")
