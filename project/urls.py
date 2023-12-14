a0dd73f5d66b540fdd1daea0860408970580263e
KCY <cks402@naver.com>
2023-12-14 14:01:23 +0900

    ♻️ 토큰 및 스키마 url 깔끔하게 조정

--- project/urls.py
+++ project/urls.py
@@ -1,37 +1,22 @@
+# Basic Django Modules
 from django.contrib import admin
 from django.urls import path, include
-from rest_framework_simplejwt.views import (
-    TokenObtainPairView,
-    TokenRefreshView,
-)
 
+# DRF-Schema Modules
 from drf_spectacular.views import (
-    SpectacularAPIView,
-    SpectacularRedocView,
-    SpectacularSwaggerView,
+    SpectacularAPIView as schema,
+    SpectacularRedocView as redoc,
+    SpectacularSwaggerView as swagger,
 )
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("accounts/", include("accounts.urls")),
     path("post/", include("post.urls")),
-    path("api-auth/", include("rest_framework.urls", namespace="rest_framework")),
-    # jwt 토큰 발급
-    path("api/token/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
-    # jwt 토큰 갱신
-    path("api/token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
     # 스키마
-    path("api/schema/", SpectacularAPIView.as_view(), name="schema"),
+    path("schema/", schema.as_view(), name="schema"),
     # 스웨거
-    path(
-        "api/schema/swagger-ui/",
-        SpectacularSwaggerView.as_view(url_name="schema"),
-        name="swagger-ui",
-    ),
+    path("schema/swagger-ui/", swagger.as_view(url_name="schema"), name="swagger-ui"),
     # 문서화
-    path(
-        "api/schema/redoc/",
-        SpectacularRedocView.as_view(url_name="schema"),
-        name="redoc",
-    ),
+    path("schema/redoc/", redoc.as_view(url_name="schema"), name="redoc"),
 ]
