4ade454d3b672cd633a293162dcdcd087efde808
KCY <cks402@naver.com>
2023-12-15 15:57:21 +0900

    ✨ 댓글 CR 완성

--- post/urls.py
+++ post/urls.py
@@ -2,6 +2,11 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.PostListView.as_view(), name="post_list"),
-    path("<int:pk>/", views.PostDetailView.as_view(), name="post_detail"),
+    path("", views.PostListCreateView.as_view(), name="post_list"),
+    path("<int:pk>/", views.PostDetailUpdateDeleteView.as_view(), name="post_detail"),
+    path(
+        "<int:pk>/comment",
+        views.CommentListCreateView.as_view(),
+        name="comment_create",
+    ),
 ]
--- post/views.py
+++ post/views.py
@@ -7,21 +7,55 @@ from rest_framework import generics
 from rest_framework import permissions
 
 # Custom Models
-from .models import Post
-from .serializers import PostSerializer
+from .models import Post, Comment
+from .serializers import PostSerializer, CommentSerializer
 from .permissions import IsAuthorOrReadOnly
 
 # User 모델을 가져옵니다.
 User = get_user_model()
 
 
-class PostListView(generics.ListCreateAPIView):
+class PostListCreateView(generics.ListCreateAPIView):
+    """
+    글 목록과 글 작성을 하는 View입니다.
+
+    method:
+    - GET: 글 목록을 출력합니다.
+    - CREATE: 글을 작성합니다.
+    """
+
     queryset = Post.objects.all()
     serializer_class = PostSerializer
     permission_classes = [permissions.IsAuthenticatedOrReadOnly]
 
 
-class PostDetailView(generics.RetrieveUpdateDestroyAPIView):
+class PostDetailUpdateDeleteView(generics.RetrieveUpdateDestroyAPIView):
+    """
+    글 자세히보기와 수정, 삭제를 합니다.
+
+    method:
+    - GET: 글 자세히보기입니다.
+    - PATCH: 글을 수정합니다.
+    - DELETE: 글을 삭제합니다.
+    """
+
     queryset = Post.objects.all()
     serializer_class = PostSerializer
     permission_classes = [IsAuthorOrReadOnly]
+
+
+class CommentListCreateView(generics.ListCreateAPIView):
+    """
+    댓글 목록과 댓글 작성을 하는 View입니다.
+
+    method:
+    - GET: 댓글 목록을 출력합니다.
+    - CREATE: 댓글을 작성합니다.
+    """
+
+    queryset = Comment.objects.all()
+    serializer_class = CommentSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+
+    def get_queryset(self):
+        return self.queryset.filter(post_id=self.kwargs["post_id"])
