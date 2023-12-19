e93680cc0fed5a1103d5ade469ec85f1cb6eba38
KimDongHu <ehdgn1022@naver.com>
2023-12-19 13:37:20 +0900

    ♻️ 채팅내역 DB 저장으로 변경

--- aichat/views.py
+++ aichat/views.py
@@ -9,7 +9,6 @@ import os
 load_dotenv()
 openai.api_key = os.getenv("OPENAI_API_KEY")
 
-
 class ChatViewSet(viewsets.ModelViewSet):
     """
     AI와 채팅을 할 수 있는 Class View 입니다.
@@ -17,26 +16,22 @@ class ChatViewSet(viewsets.ModelViewSet):
     method:
     - GET: 현재 접속된 유저의 채팅 목록을 보여줍니다.
     - POST: prompt를 작성하고 ai의 답변을 받습니다.
+    - DELETE: 현재 접속된 유저의 채팅을 모두 삭제합니다.
     """
-
     queryset = Conversation.objects.all()
     serializer_class = ConversationSerializer
 
     def get_queryset(self):
-        return Conversation.objects.filter(author=self.request.user)
+        user = self.request.user
+        return Conversation.objects.filter(author=user)
 
     def create(self, request, *args, **kwargs):
-        print(request.data)
         prompt = request.data.get("prompt")
         if prompt:
             author = self.request.user
-            previous_conversations = "\n".join(
-                [
-                    f"User: {c['prompt']}\nAI: {c['response']}"
-                    for c in request.data.get("conversations", [])
-                ]
-            )
-            prompt_with_previous = f"{previous_conversations}\nUser: {prompt}\nAI:"
+            previous_conversations = Conversation.objects.filter(author=author)
+            conversation_text = "\n".join([f"User: {c.prompt}\nAI: {c.response}" for c in previous_conversations])
+            prompt_with_previous = f"{conversation_text}\nUser: {prompt}\nAI:"
 
             model_engine = "text-davinci-003"
             completions = openai.Completion.create(
