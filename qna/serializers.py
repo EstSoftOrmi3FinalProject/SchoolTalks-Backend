26d3107485728f9988b7c61896de10d6a03ee410
maxkim77 <141907655+maxkim77@users.noreply.github.com>
2023-12-22 17:08:58 +0900

    :fire: 재 업로드를 위한삭제

--- qna/serializers.py
+++
@@ -1,8 +0,0 @@
-from rest_framework import serializers
-from .models import Inquiry
-
-
-class InquirySerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Inquiry
-        fields = ["id", "email", "subject", "content"]
