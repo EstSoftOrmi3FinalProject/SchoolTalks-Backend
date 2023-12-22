7273593dd070ebcb42c2ed89bf57e5e580e0cb34
maxkim77 <141907655+maxkim77@users.noreply.github.com>
2023-12-22 17:08:47 +0900

    :fire: 재 업로드를 위한삭제

--- chat/serializers.py
+++
@@ -1,9 +0,0 @@
-#  serializers.py
-from rest_framework import serializers
-from .models import ChatMessage
-
-
-class ChatMessageSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = ChatMessage
-        fields = "__all__"
