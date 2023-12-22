177af1b53b97751a00089d91374c40feb9514736
maxkim77 <141907655+maxkim77@users.noreply.github.com>
2023-12-22 17:59:07 +0900

    :fire: 재업로드를 위한 폴더 삭제

--- chat/migrations/models.py
+++
@@ -1,9 +0,0 @@
-from django.db import models
-
-
-class ChatMessage(models.Model):
-    message = models.TextField()
-    timestamp = models.DateTimeField(auto_now_add=True)
-
-    def __str__(self):
-        return f"ChatMessage {self.id}"
