33ddcc7af3624583aa3fd03f25f9d79a619ca749
maxkim77 <141907655+maxkim77@users.noreply.github.com>
2023-12-22 17:56:38 +0900

    :bug: 파일 추가

---
+++ chat/migrations/urls.py
@@ -0,0 +1,12 @@
+# urls.py
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    # DRF 뷰 URL 패턴
+    path(
+        "api/chat-messages/",
+        views.ChatMessageListCreateView.as_view(),
+        name="chat-message-list-create",
+    ),
+]
