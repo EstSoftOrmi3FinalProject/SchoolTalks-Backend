6ff4253eef073445c50b45bfa68ada0b1ec99c37
maxkim77 <141907655+maxkim77@users.noreply.github.com>
2023-12-22 17:59:32 +0900

    Add files via upload

---
+++ chat/migrations/0001_initial.py
@@ -0,0 +1,21 @@
+from django.db import migrations, models
+
+
+class Migration(migrations.Migration):
+
+
+    initial = True
+
+    dependencies = [
+    ]
+
+    operations = [
+        migrations.CreateModel(
+            name='ChatMessage',
+            fields=[
+                ('id', models.BigAutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
+                ('message', models.TextField()),
+                ('timestamp', models.DateTimeField(auto_now_add=True)),
+            ],
+        ),
+    ]
